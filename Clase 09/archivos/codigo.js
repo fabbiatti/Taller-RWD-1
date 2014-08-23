28332e09f372979ab62e90adb3f7260106c54fe2
Facundo <info@facundoabbiatti.com.ar>
2014-08-23 18:58:57 -0300

    Clase 09
    Introducción a JavaScript

---
+++ Clase 09/archivos/codigo.js
@@ -0,0 +1,9 @@
+//alert: Función
+// (): Parámetros de la función
+// "": Cadena de texto
+var nombre = "Marcelo";
+var apellido = "Tinelli";
+var edad = 45;
+
+//alert(nombre + " " + apellido + "\n" + edad + " años.");
+alert("2"+5*8);
